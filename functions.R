b5664f9b1fe414dd04f184defae8565170e165ca
ttomench <tracetomenchok@yahoo.com>
2016-04-15 17:38:58 -0600

    The functions are built. I don't know if they work well yet...

--- functions.R
+++ functions.R
@@ -13,22 +13,36 @@ pow=function(t1,t2,t3,h){
 }
 
 lin.bound=function(t1,t2,t3,h){
-  if(h==0){0}
-  if(h<t3 & h>0){if(t1==0){(t2/t3)*h}else{t1+(t2/t3)*h}}
-  else{if(t1==0){t2}else{t1+t2}}
+  #if(h==0){0}
+  if(h<t3 & h>0){
+    if(t1==0){(t2/t3)*h}
+    else{t1+(t2/t3)*h}
+    }
+  else{
+    if(t1==0){t2}
+    else{t1+t2}
+    }
 }
 
 circular=function(t1,t2,t3,h){
-  if(h==0){0}
-  if(h<t3 & h>0){if(t1==0){t2*(1-(2/pi)*arccos(h/t3)+(2*h/(pi*t3))*sqrt(1-(h/t3)^2))}else{t1+t2*(1-(2/pi)*arccos(h/t3)+(2*h/(pi*t3))*sqrt(1-(h/t3)^2))}}
-  else{if(t1==0){t2}else{t1+t2}}
+  #if(h==0){0}
+  if(h<t3 & h>0){
+    ifelse(t1==0,t2*(1-(2/pi)/cos(h/t3)+(2*h/(pi*t3))*sqrt(1-(h/t3)^2)),t1+t2*(1-(2/pi)/cos(h/t3)+(2*h/(pi*t3))*sqrt(1-(h/t3)^2)))
+  }
+  else{
+    ifelse(t1==0,t2,t1+t2)
+    }
   
 }
 
 spherical=function(t1,t2,t3,h){
-  if(h==0){0}
-  if(h<t3 & h>0){if(t1==0){t2*((1.5*h/t3)-(0.5*(h/t3)^3))}else{t1+t2*((1.5*h/t3)-(0.5*(h/t3)^3))}}
-  else{if(t1==0){t2}else{t1+t2}}
+  #if(h==0){0}
+  if(h<t3 & h>0){
+    ifelse(t1==0,t2*((1.5*h/t3)-(0.5*(h/t3)^3)),t1+t2*((1.5*h/t3)-(0.5*(h/t3)^3)))
+    }
+  else{
+    ifelse(t1==0,t2,t1+t2)
+    }
   
 }
 
@@ -53,27 +67,162 @@ wave=function(t1,t2,t3,h){
 }
 
 # Calculates Weighted least squares for optim function
-WRSS=function(thetas){
+WRSS=function(thetas,func,vg){
+  if(func=="linear"){
+    gam.theta=lin(thetas[1],thetas[2],vg[,2])
+  }
+  if(func=="power"){
+    gam.theta=pow(thetas[1],thetas[2],thetas[3],vg[,2])
+  }
+  if(func=="linear bound"){
+    gam.theta=lin.bound(thetas[1],thetas[2],thetas[3],vg[,2])
+  }
+  if(func=="circular"){
+    gam.theta=circular(thetas[1],thetas[2],thetas[3],vg[,2])
+  }
+  if(func=="spherical"){
+    gam.theta=spherical(thetas[1],thetas[2],thetas[3],vg[,2])
+  }
+  if(func=="rational quadratic"){
+    gam.theta=rational.quadratic(thetas[1],thetas[2],thetas[3],vg[,2])
+  }
+  if(func=="exponential"){
+    gam.theta=exponential(thetas[1],thetas[2],thetas[3],vg[,2])
+  }
+  if(func=="gaussian"){
+    gam.theta=gauss(thetas[1],thetas[2],thetas[3],vg[,2])
+  }
+  if(func=="wave"){
+    gam.theta=wave(thetas[1],thetas[2],thetas[3],vg[,2])
+  }
   #thetas = parameter vector
-  gam.theta=func(thetas[1],thetas[2],thetas[3],vg[,2])
+  #gam.theta=func(thetas[1],thetas[2],thetas[3],vg[,2])
   sum((vg[,1]/(gam.theta^2))*((vg[,3]-gam.theta)^2))
 }
 
 
 
 #Estimate the parameters
-params.smvg.model=function(smvg,inital.param,model){
-  vg=smvg
-  func=model
-  param=optim(initial.param,WRSS)$par
+params.smvg.model=function(smvg,initial.param,model){
+  param=optim(initial.param,WRSS,func=model,vg=smvg)$par
   return(param)
 }
 
 # Plot the Semivariogram
-plot.smvg.model=function(vg,param,model,new.h,title,xlimit,ylimit){
-  func=model
+plot.smvg.model=function(vg,param,model,new.h,xlimit,ylimit){
+  if(model=="linear"){
+    title = "Linear Model"
+    new=lin(param[1],param[2],new.h)
+  }
+  if(model=="power"){
+    title = "Power Model"
+    new=pow(param[1],param[2],param[3],new.h)
+  }
+  if(model=="linear bound"){
+    title = "Linear Bound Model"
+    new=lin.bound(param[1],param[2],param[3],new.h)
+  }
+  if(model=="circular"){
+    title = "Circular Model"
+    new=circular(param[1],param[2],param[3],new.h)
+  }
+  if(model=="spherical"){
+    title = "Spherical Model"
+    new=spherical(param[1],param[2],param[3],new.h)
+  }
+  if(model=="rational quadratic"){
+    title = "Rational Quadratic Model"
+    new=rational.quadratic(param[1],param[2],param[3],new.h)
+  }
+  if(model=="exponential"){
+    title = "Exponential Model"
+    new=exponential(param[1],param[2],param[3],new.h)
+  }
+  if(model=="gaussian"){
+    title = "Gaussian Model"
+    new=gauss(param[1],param[2],param[3],new.h)
+  }
+  if(model=="wave"){
+    title = "Wave Model"
+    new=wave(param[1],param[2],param[3],new.h)
+  }
   
   plot(vg[,2],vg[,3],pch=19,col=1,xlab="h",ylab=expression(paste("Estimated ",gamma(h))), main=title,xlim=xlimit,ylim=ylimit)
-  new.exp=func(param.exp[1],param.exp[2],new.h)
-  lines(new.h,new.exp,lwd=3)
+  lines(new.h,new,lwd=3)
+}
+
+choose.model= function(smvg,initial.param){
+  bic.calc = array()
+  K = length(smvg[,1])
+  mod=""
+  param=c(0,0,0)
+  
+  param.lin = params.smvg.model(smvg,initial.param,"linear")
+  bic.calc[1] = K*log(WRSS(param.lin,"linear",smvg)/K)+2*2
+  
+  param.pow=params.smvg.model(smvg,initial.param,"power")
+  bic.calc[2] = K*log(WRSS(param.pow,"power",smvg)/K)+2*2
+  
+  param.lin.bou= params.smvg.model(smvg,initial.param,"linear bound")
+  bic.calc[3] = K*log(WRSS(param.lin.bou,"linear bound",smvg)/K)+2*2
+  
+  param.circ=params.smvg.model(smvg,initial.param,"circular")
+  bic.calc[4] = K*log(WRSS(param.circ,"circular",smvg)/K)+2*2
+  
+  param.spher=params.smvg.model(smvg,initial.param,"spherical")
+  bic.calc[5] = K*log(WRSS(param.spher,"spherical",smvg)/K)+2*2
+  
+  param.rat.quad=params.smvg.model(smvg,initial.param,"rational quadratic")
+  bic.calc[6] = K*log(WRSS(param.rat.quad,"rational quadratic",smvg)/K)+2*2
+  
+  param.exp=params.smvg.model(smvg,initial.param,"exponential")
+  bic.calc[7] = K*log(WRSS(param.exp,"exponential",smvg)/K)+2*2
+  
+  param.gaus=params.smvg.model(smvg,initial.param,"gaussian")
+  bic.calc[8] = K*log(WRSS(param.gaus,"gaussian",smvg)/K)+2*2
+  
+  param.wa=params.smvg.model(smvg,initial.param,"wave")
+  bic.calc[9] = K*log(WRSS(param.wa,"wave",smvg)/K)+2*2
+  
+  BIC.which=which.min(bic.calc)
+  BIC=bic.calc[BIC.which]
+  
+  if(BIC==1){
+    model="linear"
+    param=param.lin
+  }
+  if(BIC==2){
+    model="power"
+    param=param.pow
+  }
+  if(BIC==1){
+    model="linear bound"
+    param=param.lin.bou
+  }
+  if(BIC==1){
+    model="circular"
+    param=param.circ
+  }
+  if(BIC==1){
+    model="spherical"
+    param=param.spher
+  }
+  if(BIC==1){
+    model="rational quadratic"
+    param=param.rat.quad
+  }
+  if(BIC==1){
+    model="exponential"
+    param=param.exp
+  }
+  if(BIC==1){
+    model="gaussian"
+    param=param.gaus
+  }
+  if(BIC==1){
+    model="wave"
+    param=param.wa
+  }
+  best.model=cbind(model,param,BIC)
+  return(best.model)
 }
--- trace's_code.R
+++ trace's_code.R
@@ -1,5 +1,6 @@
-#setwd("Z:/Practicum")
-setwd("~/Documents/Practicum")
+rm(list = ls())
+#setwd("Z:/Practicum/Spatial_Project_2016/Spatial_Project_2016")
+setwd("~/Documents/Practicum/Spatial_Project_2016")
 load("predictors.Rdata")
 library(fields)
 library(maps)
@@ -9,4 +10,146 @@ library(geoR)
 library(sp)
 library(maptools)
 
+
+
+#------maps for each month & ptype (36 individual plots)------#
+#first obtain prior probs based on observed frequency
+
+months = as.numeric(substr(dates,5,6))
+pi.snow = matrix(nrow = 551, ncol = 9)
+pi.rain = matrix(nrow = 551, ncol = 9)
+pi.ip = matrix(nrow = 551, ncol = 9)
+pi.fzra = matrix(nrow = 551, ncol = 9)
+
+for(i in 1:551){ 
+  #i = station
+  station.indicies = which(station.ind == i) #indices of station i
+  
+  for(j in 1:length(unique(months))){ 
+    #j = month
+    
+    #There are some missing probabilities for May and September, so we use the observations from April and October to record frequency:
+    if(j == 5){m = sort(unique(months))[j-1]}else{if(j == 6){m = sort(unique(months))[j+1]}else{m = sort(unique(months))[j]}}  
+    
+    month.ind = which(months == m) 
+    month.indicies = which(date.ind%in%month.ind)
+    total.month.station = intersect(month.indicies, station.indicies)
+    
+    snow.indicies = which(ptype[total.month.station] == "SN")
+    rain.indicies = which(ptype[total.month.station] == "RA")
+    ip.indicies = which(ptype[total.month.station] == "IP")
+    fzra.indicies = which(ptype[total.month.station] == "FZRA")
+    
+    pi.snow[i,j] = length(snow.indicies)/length(total.month.station)
+    pi.rain[i,j] = length(rain.indicies)/length(total.month.station)
+    pi.ip[i,j] = length(ip.indicies)/length(total.month.station)
+    pi.fzra[i,j] = length(fzra.indicies)/length(total.month.station)
+    
+  }
+}
+
 lon.new = lon-360
+lon.lat=cbind(lon.new,lat)
+dim(lon.lat)
+
+
+#colnames(lon.lat)
+utm=project(lon.lat,proj="+proj=utm +zone=0016 ellps=WGS84")
+#dim(utm)
+
+#par(mfrow=c(1,2))
+#plot(lon.lat[,1],lon.lat[,2],pch=20);title('Longitute/Latitude Locations')
+#plot(utm[,1],utm[,2],pch=20,col=4);title('Utm Locations')
+
+#Euclid.utm=as.matrix(dist(utm))					#Euclidean distances between utm coordinates
+GCD.latlon=rdist.earth(lon.lat,miles=FALSE)		#Great circle distances between lat/long coordinates
+max(GCD.latlon)/3
+d=GCD.latlon
+#plot(Euclid.utm);title('Euclidean Distance')
+#plot(GCD.latlon):title('Great Circle Distance')
+#xy=cbind(lon.new,lat)
+#d=as.matrix(dist(xy))
+#max(d)/3
+
+for(i in 1:9){
+  
+  m = sort(unique(months))[i]
+  pdf(file = paste("figures/Priors/prior_", m,".pdf", sep=""))
+  par(mfrow = c(2,2))
+  quilt.plot(lon.lat, pi.snow[,i],zlim = c(0,1),main = paste("Snow Probabilities, Month",m , sep = ""))
+  map("world", add = T)
+  map("state", add = T)
+  
+  quilt.plot(lon.lat, pi.rain[,i], zlim = c(0,1),main = paste("Rain Probabilities, Month",m , sep = ""))
+  map("world", add = T)
+  map("state", add = T)
+  
+  quilt.plot(lon.lat, pi.ip[,i], zlim = c(0,0.3),main = paste("Ice Pellet Probabilities, Month",m , sep = ""))
+  map("world", add = T)
+  map("state", add = T)
+  
+  quilt.plot(lon.lat, pi.fzra[,i], zlim = c(0,0.3),main = paste("Freezing Rain Probabilities, Month",m , sep = ""))
+  map("world", add = T) 
+  map("state", add = T)
+  dev.off()
+  
+  #somethng is wrong with this dev.off() thing. this works when we do it one by one, maybe we should just do it that way.
+  pdf(file = paste("figures/Semivariograms/smvg_", m,".pdf", sep=""))
+  par(mfrow=c(1,4))
+  #pdf(file = paste("figures/Semivariograms/snow_", m,".pdf", sep=""))
+  probs.snow = as.data.frame(cbind(lon.lat, pi.snow[,i]))
+  coordinates(probs.snow) = ~lon.new +lat
+  vg.snow=variogram(pi.snow[,i]~1, data=probs.snow,cutoff=max(d)/2,width=2)
+  plot(vg.snow,pch=19,col=1,ylab=expression(paste("Estimated ",gamma(h))), main = paste("Snow in Month",m, sep = " ")) 
+  #dev.off()
+  
+  #pdf(file = paste("figures/Semivariograms/rain_", m,".pdf", sep=""))
+  probs.rain = as.data.frame(cbind(lon.new, lat, pi.rain[,i]))
+  coordinates(probs.rain) = ~lon.new +lat
+  vg.rain=variogram(pi.rain[,i]~1, data=probs.rain,cutoff=max(d)/2,width=2) #cutoff=max(d)/2,width=2)
+  plot(vg.rain,pch=19,col=1,ylab=expression(paste("Estimated ",gamma(h))), main = paste("Rain in Month",m, sep = " "))
+  #dev.off()
+  
+  #pdf(file = paste("figures/Semivariograms/ip_", m,".pdf", sep=""))
+  probs.ip = as.data.frame(cbind(lon.new, lat, pi.ip[,i]))
+  coordinates(probs.ip) = ~lon.new +lat
+  vg.ip=variogram(pi.ip[,i]~1, data=probs.ip,cutoff=max(d)/2,width=2) #cutoff=max(d)/2,width=2)
+  plot(vg.ip,pch=19,col=1,ylab=expression(paste("Estimated ",gamma(h))), main = paste("Ice Pellets in Month",m, sep = " "))
+  #dev.off()
+  
+  #pdf(file = paste("figures/Semivariograms/fzra_", m,".pdf", sep=""))
+  probs.fzra = as.data.frame(cbind(lon.new, lat, pi.fzra[,i]))
+  coordinates(probs.fzra) = ~lon.new +lat
+  vg.fzra=variogram(pi.fzra[,i]~1, data=probs.fzra,cutoff=max(d)/2,width=2)
+  plot(vg.fzra,pch=19,col=1,ylab=expression(paste("Estimated ",gamma(h))), main = paste("Freezing Rain in Month",m, sep = " "))
+  dev.off()
+  
+  print(i)
+}
+
+initial.params = c(0.01,0.15,20)
+params = params.smvg.model(vg.snow,initial.params,"linear")
+
+new.h=seq(0,50,len=1000)
+plot.smvg.model(vg.snow,initial.params,"linear",new.h,c(0,50),c(0,0.18))
+
+choose.model(vg.snow,initial.params)
+#just trying this outside the loop
+#Fit a wave model
+probs.snow = as.data.frame(cbind(lon.new, lat, pi.snow[,i]))
+coordinates(probs.snow) = ~lon.new +lat
+vg.snow=variogram(pi.snow[,i]~1, data=probs.snow,cutoff=max(d)/2,width=4)
+initial=vgm(psill=2.4,model="Wav",range=11)
+fit.vg=fit.variogram(vg.snow,initial,fit.method=2)
+plot(vg.snow, fit.vg, col = "black", pch = 19, lwd = 3, ylab=expression(paste("Estimated ",gamma(h))), main = "Wave Fit w/o Nugget")
+#so basically the plots aren't showing up ! but we would theoretically do this for every ptype and month
+
+
+#using the smvg model: krig at each station location (leave one out X val)
+#Thin plate spline 
+#compare krig & thin plate spline
+#go through and check probabilities (> 0, sum =1)
+
+
+###could be cool to plot all 4 ptype smvg's on one plot for each month:
+#https://stat.ethz.ch/pipermail/r-sig-geo/2009-September/006404.html
